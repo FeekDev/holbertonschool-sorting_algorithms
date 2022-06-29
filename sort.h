146601b4a4e60c39907a5ed613ca525054ce1c63
felipeholbi <4758@holbertonschool.com>
2022-06-29 18:17:43 -0500

    fix_sort

--- sort.h
+++ sort.h
@@ -24,4 +24,8 @@ void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swap(int *left, int *right);
+void selection_sort(int *array, size_t size);
+size_t partition(int *array, ssize_t low, ssize_t high, size_t size);
+void q_sort(int *array, ssize_t low, ssize_t high, size_t size);
+void quick_sort(int *array, size_t size);
 #endif /*_SORT_H_*/
